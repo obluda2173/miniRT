d8d8fe449cbed33501f035ce25d19e71475b212c
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-01 15:45:12 +0100

    sphere.rt renders perfectly

--- src/raytracing.c
+++ src/raytracing.c
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 10:51:38 by erian             #+#    #+#             */
-/*   Updated: 2025/03/01 14:57:03 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/01 15:43:23 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,28 @@
 
 t_ray	generate_ray(t_cam *camera, int x, int y)
 {
-    t_ray	ray;
-    double	aspect_ratio;
-    double	fov_scale;
-    t_vec	w;
-    t_vec	u;
-    t_vec	v;
-    t_vec	horizontal;
-    t_vec	vertical;
-    t_vec	lower_left_corner;
+	t_ray	ray;
+	double	aspect_ratio = (double)WIN_WIDTH / WIN_HEIGHT;
+	double	fov_scale = tan(camera->fov / 2.0 * (M_PI / 180.0));
 
-    aspect_ratio = (double)WIN_WIDTH / (double)WIN_HEIGHT;
-    fov_scale = tan(camera->fov / 2.0 * (M_PI / 180.0));
+	// Basis vectors
+	t_vec	w = normalize(scale(camera->orientation, -1));
+	t_vec	u = normalize(cross(w, vec(0, 1, 0)));
+	t_vec	v = cross(u, w);
 
-    // Calculate camera basis vectors
-    w = normalize(scale(camera->orientation, -1));
-    u = normalize(cross(vec(0, 1, 0), w)); // Right
-    v = cross(w, u); // Up
+	// Viewport dimensions
+	t_vec	horizontal = scale(u, 2.0 * aspect_ratio * fov_scale);
+	t_vec	vertical = scale(v, 2.0 * fov_scale);
+	t_vec	lower_left_corner = sub(sub(sub(camera->coordinates, scale(horizontal, 0.5)), scale(vertical, 0.5)), w);
 
-    horizontal = scale(u, 2.0 * aspect_ratio * fov_scale);
-    vertical = scale(v, 2.0 * fov_scale);
-    lower_left_corner = sub(sub(sub(camera->coordinates, scale(horizontal, 0.5)), scale(vertical, 0.5)), w);
+	// Ray calculation
+	ray.origin = camera->coordinates;
+	t_vec offset = add(scale(horizontal, (double)x/WIN_WIDTH), 
+						scale(vertical, (double)y/WIN_HEIGHT));
+	t_vec point_on_viewport = add(lower_left_corner, offset);
+	ray.direction = normalize(sub(point_on_viewport, ray.origin));
 
-    ray.origin = camera->coordinates;
-    ray.direction.x = ((double)x / WIN_WIDTH) * horizontal.x + ((double)y / WIN_HEIGHT) * vertical.x - lower_left_corner.x;
-    ray.direction.y = ((double)x / WIN_WIDTH) * horizontal.y + ((double)y / WIN_HEIGHT) * vertical.y - lower_left_corner.y;
-    ray.direction.z = ((double)x / WIN_WIDTH) * horizontal.z + ((double)y / WIN_HEIGHT) * vertical.z - lower_left_corner.z;
-    ray.direction = normalize(ray.direction);
-
-    return (ray);
+	return (ray);
 }
 
 bool	ray_sphere_intersect(t_ray ray, t_sphere *sphere, double *t)
@@ -69,7 +62,7 @@ bool	ray_sphere_intersect(t_ray ray, t_sphere *sphere, double *t)
 	t1 = root_n(a, b, c);
 	t2 = root_p(a, b, c);
 	if (t1 > EPSILON && t2 > EPSILON)
-    	*t = fmin(t1, t2);
+		*t = fmin(t1, t2);
 	else if (t1 > EPSILON)
 		*t = t1;
 	else if (t2 > EPSILON)
@@ -89,10 +82,10 @@ void	render_scene(t_data *data)
 
 	sphere = (t_sphere *)get_specific_obj(data->scene->obj_lst, SPHERE);
 	if (!sphere)
-    {
-        ft_putstr_fd("Error: Sphere not found\n", STDERR_FILENO);
-        return;
-    }
+	{
+		ft_putstr_fd("Error: Sphere not found\n", STDERR_FILENO);
+		return;
+	}
 	y = 0;
 	while (y < WIN_HEIGHT)
 	{

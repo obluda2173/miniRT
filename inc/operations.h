624b83303d8a4ff1ca01be3f31e2ab7147aba1fe
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-08 11:29:25 +0100

    comments in math header

--- inc/operations.h
+++ inc/operations.h
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 11:06:21 by erian             #+#    #+#             */
-/*   Updated: 2025/03/08 10:21:03 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/08 11:28:57 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,21 @@
 # define M_PI		3.14159265358979323846
 # define EPSILON	1e-6
 
+// math_operations_1.c
 t_vec	vec(double x, double y, double z);
 t_vec	add(t_vec v1, t_vec v2);
 t_vec	sub(t_vec v1, t_vec v2);
 t_vec	cross(t_vec v1, t_vec v2);
 t_vec	scale(t_vec v, double scalar);
-double	dot(t_vec v1, t_vec v2);
 
+// math_operations_2.c
+double	dot(t_vec v1, t_vec v2);
 double	length(t_vec v);
 double	discr(double a, double b, double c);
 double	root_n(double a, double b, double c);
 double	root_p(double a, double b, double c);
+
+// math_operations_3.c
 t_vec	normalize(t_vec v);
 t_vec	reflect(t_vec v, t_vec normal);
 

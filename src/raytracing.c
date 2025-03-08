dfba88b800b46cff10c9442ba29dad32fbadfb33
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-08 15:23:29 +0100

    norming raytracing

--- src/raytracing.c
+++ src/raytracing.c
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 10:51:38 by erian             #+#    #+#             */
-/*   Updated: 2025/03/08 14:58:13 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/08 15:22:09 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,67 +45,60 @@ t_ray	generate_ray(t_cam *camera, t_cam_settings cam_set, int x, int y)
 	ray.direction = normalize(sub(point_on_viewport, ray.origin));
 	return (ray);
 }
-// specific_obj, inter, 
-t_color	apply_checkerboard(t_intersection *inter, t_color color)
+
+t_color handle_plane(t_intersection *inter, t_obj *obj)
 {
-	if (((int)floor(inter->hit_point.x * 2)
-			+ (int)floor(inter->hit_point.z * 2)) % 2 == 0)
-		return (color);
+	t_plane *plane = (t_plane *)obj->specific_obj;
+	double u, v;
+	t_color color;
+
+	compute_plane_uv(inter->hit_point, plane, &u, &v);
+	if (plane->texture && plane->normal_map)
+	{
+		color = sample_xpm(plane->texture, u, v);
+		t_vec bump = sample_normal_map(plane->normal_map, u, v);
+		t_vec rot_axis = normalize(cross(plane->normal_vector, bump));
+		double rot_angle = acos(dot(plane->normal_vector, bump));
+		inter->normal = add(add(scale(bump, cos(rot_angle)), scale(cross(rot_axis, bump), sin(rot_angle))), scale(rot_axis, dot(rot_axis, bump) * (1 - cos(rot_angle))));
+	}
 	else
-		return (color_scale(color, 0.8));
+	{
+		color = plane->color;
+	}
+
+	return (color);
+	// if (((int)floor(inter->hit_point.x * 2) + (int)floor(inter->hit_point.z * 2)) % 2 == 0)
+	// 	return color;
+	// else
+	// 	return color_scale(color, 0.8);
 }
 
-int	process_pixel(t_data *data, t_cam_settings cam_set, int x, int y)
+int process_pixel(t_data *data, t_cam_settings cam_set, int x, int y)
 {
-	double			t;
-	t_ray			ray;
-	t_vec			normal;
-	t_obj			*closest_obj;
-	t_intersection	inter;
-	int				color;
-	double			u, v;
+	double          t;
+	t_ray           ray;
+	t_vec           normal;
+	t_obj           *closest_obj;
+	t_intersection  inter;
 
 	ray = generate_ray(data->scene->camera, cam_set, x, y);
 	closest_obj = find_closest_object(ray, data->scene->obj_lst, &t, &normal);
-
 	if (!closest_obj)
 		return (0x000000);
-
 	inter.hit_point = add(ray.origin, scale(ray.direction, t));
 	if (dot(normal, ray.direction) > 0)
 		normal = scale(normal, -1);
 	inter.normal = normal;
-
 	if (closest_obj->type == SPHERE)
 		inter.base_color = ((t_sphere *)closest_obj->specific_obj)->color;
-	if (closest_obj->type == PLANE)
-	{
-		t_plane *plane = (t_plane *)closest_obj->specific_obj;
-		compute_plane_uv(inter.hit_point, plane, &u, &v);
-		if (plane->texture && plane->normal_map)
-		{
-			inter.base_color = sample_xpm(plane->texture, u, v);
-			// inter.normal = sample_normal_map(plane->normal_map, u, v);
-
-			t_vec bump = sample_normal_map(plane->normal_map, u, v);
-			t_vec rot_axis = normalize(cross(plane->normal_vector, bump));
-			double rot_angle = acos(dot(plane->normal_vector, bump));
-			inter.normal = add(add(scale(bump, cos(rot_angle)), scale(cross(rot_axis, bump), sin(rot_angle))), scale(rot_axis, dot(rot_axis, bump) * (1 - cos(rot_angle))));
-		}
-		else
-			inter.base_color = plane->color;
-		// else
-			// inter.base_color = apply_checkerboard(&inter, plane->color);
-	}
-	if (closest_obj->type == CYLINDER)
+	else if (closest_obj->type == PLANE)
+		inter.base_color = handle_plane(&inter, closest_obj);
+	else if (closest_obj->type == CYLINDER)
 		inter.base_color = ((t_cylinder *)closest_obj->specific_obj)->color;
-	if (closest_obj->type == CONE)
+	else if (closest_obj->type == CONE)
 		inter.base_color = ((t_cone *)closest_obj->specific_obj)->color;
-
-	color = apply_ambient_light(inter.base_color, data->scene->a_light);
-	color = apply_source_light(data->scene, &inter, color);
-
-	return (color);
+	return (apply_source_light(data->scene, &inter,
+		apply_ambient_light(inter.base_color, data->scene->a_light)));
 }
 
 void	render_scene(t_data *data)

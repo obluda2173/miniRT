5234e4a5cbdbe6661d9d567c760cc3805016d776
Kay Freyer <kfreyer@c1r6s4.42wolfsburg.de>
2025-03-08 16:30:00 +0100

    cone intersection normed

--- src/ray_cone_intersection.c
+++ src/ray_cone_intersection.c
@@ -1,6 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_cone_intersection.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfreyer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 16:28/56 by kfreyer           #+#    #+#             */
+/*   Updated: 2025/03/08 16:28:56 by kfreyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "operations.h"
 #include "raytracing.h"
 #include "structures.h"
-#include "operations.h"
 
 /*
 X = O - C
@@ -11,22 +22,36 @@ d2 = <dp | v_a>
 
 A = cos(alpha)^2 * <a_part | a_part> - sin(alpha)^2 * d1^2
 */
-bool ray_inf_cone_intersect(t_ray ray, t_cone *cone, double *t) {
-	t_quadratic_coeff coeff;
-	t_vec X = sub(ray.origin, cone->apex);
-	double one_plus_k_squared = (1 + (tan(cone->alpha) * tan(cone->alpha)));
+t_quadratic_coeff	calc_coeff(t_ray ray, t_cone *cone)
+{
+	t_quadratic_coeff	coeff;
+	t_vec				dp;
+	double				one_plus_k_squared;
 
-	coeff.a = dot(ray.direction, ray.direction) -  one_plus_k_squared * dot(ray.direction, cone->axis) * dot(ray.direction, cone->axis);
-	coeff.b = 2 * (dot(ray.direction, X) -  one_plus_k_squared * dot(ray.direction, cone->axis) * dot(X, cone->axis));
-	coeff.c = dot(X, X) - one_plus_k_squared * dot(X, cone->axis) * dot(X, cone->axis);
+	dp = sub(ray.origin, cone->apex);
+	one_plus_k_squared = (1 + (tan(cone->alpha) * tan(cone->alpha)));
+	coeff.a = dot(ray.direction, ray.direction) - one_plus_k_squared
+		* dot(ray.direction, cone->axis) * dot(ray.direction, cone->axis);
+	coeff.b = 2 * (dot(ray.direction, dp) - one_plus_k_squared
+			* dot(ray.direction, cone->axis) * dot(dp, cone->axis));
+	coeff.c = dot(dp, dp) - one_plus_k_squared * dot(dp, cone->axis) * dot(dp,
+			cone->axis);
+	return (coeff);
+}
 
-	double discriminant = discr(coeff);
+bool	ray_inf_cone_intersect(t_ray ray, t_cone *cone, double *t)
+{
+	t_quadratic_coeff	coeff;
+	double				discriminant;
+	double				t1;
+	double				t2;
+
+	coeff = calc_coeff(ray, cone);
+	discriminant = discr(coeff);
 	if (discriminant < 0)
-		return NULL;
-
-	double t1 = root_n(coeff);
-	double t2 = root_p(coeff);
-
+		return (NULL);
+	t1 = root_n(coeff);
+	t2 = root_p(coeff);
 	if (t1 > EPSILON && t2 > EPSILON)
 		*t = fmin(t1, t2);
 	else if (t1 > EPSILON)
@@ -37,51 +62,3 @@ bool ray_inf_cone_intersect(t_ray ray, t_cone *cone, double *t) {
 		return (false);
 	return (true);
 }
-
-/*
-dp = p - pa
-a_part = v - <v | v_a> v_a
-c_part = dp - <dp | v_a> v_a
-d1 = <v | v_a>
-d2 = <dp | v_a>
-
-A = cos(alpha)^2 * <a_part | a_part> - sin(alpha)^2 * d1^2
-*/
-/* bool ray_inf_cone_intersect(t_ray ray, t_cone cone, double *t) { */
-
-/* 	t_vec dp = sub(ray.origin, cone.apex); */
-/* 	t_vec a_part = sub(ray.direction, scale(cone.apex, dot(ray.direction, cone.apex))); */
-/* 	t_vec c_part = sub(dp, scale(cone.apex, dot(dp, cone.apex))); */
-/* 	double d1 = dot(ray.origin, cone.apex); */
-/* 	double d2 = dot(dp, cone.apex); */
-
-/* 	double A = cos(cone.alpha) * cosh(cone.alpha) * dot(a_part, a_part) * */
-/* 		dot(a_part, a_part) - */
-/* 		sin(cone.alpha) * sin(cone.alpha) * d1 * d1; */
-
-/* 	double B = */
-/* 		2 * cos(cone.alpha) * cosh(cone.alpha) * dot(a_part, c_part) - */
-/* 		2 * sin(cone.alpha) * sin(cone.alpha) * d2; */
-
-/* 	double C = cos(cone.alpha) * cos(cone.alpha) * dot(c_part, c_part) * */
-/* 		dot(c_part, c_part) - */
-/* 		sin(cone.alpha) * sin(cone.alpha) * d2 * d2; */
-
-
-/* 	double discriminant = discr(A, B, C); */
-/* 	if (discriminant < 0) */
-/* 		return NULL; */
-
-/* 	double t1 = root_n(A, B, C); */
-/* 	double t2 = root_p(A, B, C); */
-
-/* 	if (t1 > EPSILON && t2 > EPSILON) */
-/* 		*t = fmin(t1, t2); */
-/* 	else if (t1 > EPSILON) */
-/* 		*t = t1; */
-/* 	else if (t2 > EPSILON) */
-/* 		*t = t2; */
-/* 	else */
-/* 		return (false); */
-/* 	return (true); */
-/* } */
